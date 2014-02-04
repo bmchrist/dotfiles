1c6b5cc39d33d6fa852fdb22ffa6e872df7347e1
Ben Christensen <bmchrist@mtu.edu>
2014-02-04 17:33:13 -0500

    Still saving the world, one keystroke at a time

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,3 +10,10 @@ alias ts="tmuxinator start"
 
 alias vbi="vim +BundleInstall +qall"
 alias phil="head < /dev/random | mutt -s \"Rand\" -- pjmiddle@mtu.edu"
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gf="git fetch"
+alias gd="git diff"
+#alias tmux="TERM=screen-256color-bce tmux"
